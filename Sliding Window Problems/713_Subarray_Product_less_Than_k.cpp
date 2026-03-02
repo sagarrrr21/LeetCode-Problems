8facde772b07ebde90c0f81a28d70bb9da8e93bf
sagarrrr21 <chauhansagarsingh21@gmail.com>
2026-03-02 20:13:27 +0530

    LeetCode Problem 713 Solution

---
+++ Sliding Window Problems/713_Subarray_Product_less_Than_k.cpp
@@ -0,0 +1,66 @@
+/*
+713. Subarray Product Less Than K
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+Given an array of integers nums and an integer k, return the number of contiguous subarrays where the product of all the elements in the subarray is strictly less than k.
+
+
+
+Example 1:
+
+Input: nums = [10,5,2,6], k = 100
+Output: 8
+Explanation: The 8 subarrays that have product less than 100 are:
+[10], [5], [2], [6], [10, 5], [5, 2], [2, 6], [5, 2, 6]
+Note that [10, 5, 2] is not included as the product of 100 is not strictly less than k.
+Example 2:
+
+Input: nums = [1,2,3], k = 0
+Output: 0
+
+
+Constraints:
+
+1 <= nums.length <= 3 * 104
+1 <= nums[i] <= 1000
+0 <= k <= 106
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Time Complexity : O(n)
+// Space Complexity : O(1) constant
+
+class Solution
+{
+public:
+    int numSubarrayProductLessThanK(vector<int> &nums, int k)
+    {
+        if (k <= 1)
+            return 0;
+        int n = nums.size();
+        int l = 0, r = 0;
+
+        long long product = 1;
+        int cnt = 0;
+
+        while (r < n)
+        {
+            product *= nums[r];
+
+            while (product >= k)
+            {
+                product /= nums[l];
+                l++;
+            }
+            cnt += (r - l + 1);
+            r++;
+        }
+        return cnt;
+    }
+};
