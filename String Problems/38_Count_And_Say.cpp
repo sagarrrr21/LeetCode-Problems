82ab05b298a2f49fea025ff95249abb031bf526f
sagarrrr21 <chauhansagarsingh21@gmail.com>
2026-03-15 22:02:53 +0530

    LeetCode Problem 38 Solution

--- String Problems/38_Count_And_Say.cpp
+++ String Problems/38_Count_And_Say.cpp
@@ -1,11 +1,5 @@
 /*
 38. Count and Say
-Solved
-Medium
-Topics
-premium lock icon
-Companies
-Hint
 The count-and-say sequence is a sequence of digit strings defined by the recursive formula:
 
 countAndSay(1) = "1"
@@ -14,16 +8,11 @@ Run-length encoding (RLE) is a string compression method that works by replacing
 
 Given a positive integer n, return the nth element of the count-and-say sequence.
 
-
-
 Example 1:
-
 Input: n = 4
-
 Output: "1211"
 
 Explanation:
-
 countAndSay(1) = "1"
 countAndSay(2) = RLE of "1" = "11"
 countAndSay(3) = RLE of "11" = "21"
@@ -31,20 +20,12 @@ countAndSay(4) = RLE of "21" = "1211"
 Example 2:
 
 Input: n = 1
-
 Output: "1"
-
 Explanation:
-
 This is the base case.
 
-
-
 Constraints:
-
 1 <= n <= 30
-
-
 Follow up: Could you solve it iteratively?
 */
 
