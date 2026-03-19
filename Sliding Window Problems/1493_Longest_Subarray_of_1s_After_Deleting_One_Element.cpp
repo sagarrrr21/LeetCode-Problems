fb400e1685583cc8bdc868a46d46b0802533d596
sagarrrr21 <chauhansagarsingh21@gmail.com>
2026-03-19 20:40:08 +0530

    LeetCode Problem 1493 Solution

---
+++ Sliding Window Problems/1493_Longest_Subarray_of_1s_After_Deleting_One_Element.cpp
@@ -0,0 +1,103 @@
+/*
+1493. Longest Subarray of 1's After Deleting One Element
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+Given a binary array nums, you should delete one element from it.
+
+Return the size of the longest non-empty subarray containing only 1's in the resulting array. Return 0 if there is no such subarray.
+
+
+
+Example 1:
+
+Input: nums = [1,1,0,1]
+Output: 3
+Explanation: After deleting the number in position 2, [1,1,1] contains 3 numbers with value of 1's.
+Example 2:
+
+Input: nums = [0,1,1,1,0,1,1,0,1]
+Output: 5
+Explanation: After deleting the number in position 4, [0,1,1,1,1,1,0,1] longest subarray with value of 1's is [1,1,1,1,1].
+Example 3:
+
+Input: nums = [1,1,1]
+Output: 2
+Explanation: You must delete one element.
+
+
+Constraints:
+
+1 <= nums.length <= 105
+nums[i] is either 0 or 1.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int find_ones(vector<int> &nums, int idx)
+    {
+        int cur = 0, max_len = 0;
+        for (int j = 0; j < nums.size(); j++)
+        {
+            if (j == idx)
+                continue;
+            if (nums[j] == 1)
+            {
+                cur++;
+                max_len = max(max_len, cur);
+            }
+            else
+                cur = 0;
+        }
+        return max_len;
+    }
+    int longestSubarray(vector<int> &nums)
+    {
+        int n = nums.size();
+        int ans = 0;
+        int zero = 0;
+        for (int i = 0; i < n; i++)
+        {
+
+            if (nums[i] == 0)
+            {
+                ans = max(ans, find_ones(nums, i));
+                zero++;
+            }
+        }
+        if (zero == 0)
+            return n - 1;
+        return ans;
+    }
+};
+
+class Solution2
+{
+public:
+    int longestSubarray(vector<int> &nums)
+    {
+        int n = nums.size();
+        int zero = 0, ans = 0;
+        int i = 0;
+        for (int j = 0; j < n; j++)
+        {
+            if (nums[j] == 0)
+                zero++;
+            while (zero > 1)
+            {
+                if (nums[i] == 0)
+                    zero--;
+                i++;
+            }
+            ans = max(ans, j - i);
+        }
+        return ans;
+    }
+};
